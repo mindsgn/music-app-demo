31e5067e64b0113abbf11327fb398b397e3346f8
mindsgn <48827267+mindsgn@users.noreply.github.com>
2023-08-13 19:30:15 +0200

    update

--- src/components/songCard/songCard.tsx
+++ src/components/songCard/songCard.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {View, Text, TouchableOpacity, ImageBackground} from 'react-native';
 import styles from './style';
 import Icon from 'react-native-vector-icons/FontAwesome5';
+import LinearGradient from 'react-native-linear-gradient';
 
 const SongCard = ({
   artist,
@@ -15,36 +16,44 @@ const SongCard = ({
   play: () => void;
 }) => {
   return (
-    <ImageBackground
-      style={styles.container}
-      source={{uri: art}}
-      resizeMode="cover">
-      <View style={styles.details}>
-        <View>
-          <View
-            style={{
-              maxWidth: 250,
-            }}>
-            <Text numberOfLines={1} style={styles.textTitle}>
-              {title}
-            </Text>
-          </View>
-          <View
-            style={{
-              maxWidth: 100,
-            }}>
-            <Text numberOfLines={1} style={styles.textArtist}>
-              {artist}
-            </Text>
-          </View>
-        </View>
-        <View>
+    <View style={styles.container}>
+      <ImageBackground
+        style={styles.imageContainer}
+        source={{uri: art}}
+        resizeMode="cover">
+        <LinearGradient
+          colors={[
+            'rgba(0, 0, 0, 0)',
+            'rgba(0, 0, 0, 0.5)',
+            'rgba(0, 0, 0, 1)',
+          ]}
+          style={styles.buttons}>
           <TouchableOpacity style={styles.button} onPress={play}>
-            <Icon name="play" size={10} color="black" />
+            <Icon name="play" size={25} color="black" />
           </TouchableOpacity>
+        </LinearGradient>
+        <View style={styles.details}>
+          <View>
+            <View
+              style={{
+                maxWidth: 250,
+              }}>
+              <Text numberOfLines={1} style={styles.textTitle}>
+                {title}
+              </Text>
+            </View>
+            <View
+              style={{
+                maxWidth: 100,
+              }}>
+              <Text numberOfLines={1} style={styles.textArtist}>
+                {artist}
+              </Text>
+            </View>
+          </View>
         </View>
-      </View>
-    </ImageBackground>
+      </ImageBackground>
+    </View>
   );
 };
 
--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -1,10 +1,11 @@
 import {createTRPCProxyClient, httpBatchLink} from '@trpc/react-query';
 import {ApiRoute} from '../../../mixo-backend/src/app';
 
+//@ts-ignore
 export const trpc = createTRPCProxyClient<ApiRoute>({
   links: [
     httpBatchLink({
-      url: 'http://192.168.1.103:8080/api',
+      url: 'https://plankton-app-5voay.ondigitalocean.app/api',
     }),
   ],
 });
